3d479b909c348be25c6a9deb44168b9005649cef
Evgeniy <evgeniygal@gmail.com>
2019-09-11 20:24:39 +0300

    done task 3-6

---
+++ hw-03/js/task-04.js
@@ -0,0 +1,20 @@
+function countTotalSalary(employees) {
+  const salerys = Object.values(employees);
+  return salerys.reduce((a, b) => a + b);
+}
+
+console.log(
+  countTotalSalary({
+    mango: 100,
+    poly: 150,
+    alfred: 80,
+  }),
+); // 330
+
+console.log(
+  countTotalSalary({
+    kiwi: 200,
+    lux: 50,
+    chelsy: 150,
+  }),
+); // 400
