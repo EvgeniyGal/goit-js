d8ab6e361ccfb546abbe03e8126ad6faebc926cb
Evgeniy <evgeniygal@gmail.com>
2019-09-23 20:58:55 +0300

    Start hw-06

---
+++ hw-06/js/task-01.js
@@ -0,0 +1,25 @@
+import users from './users';
+
+// const users = require('./users');
+
+// console.log(users);
+// const getUserNames = (users) => {
+//   // твой код
+// };
+// Получить массив имен всех пользователей (поле name).
+
+// console.log(getUserNames(users));
+// // [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
+// Задание 2
+
+// console.group('Task-01');
+
+// console.log(Account.prototype.getInfo); // function
+
+// const mango = new Account('Mangozedog', 'mango@dog.woof');
+// mango.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
+
+// const poly = new Account('Poly', 'poly@mail.com');
+// poly.getInfo(); // Login: Poly, Email: poly@mail.com
+
+// console.groupEnd();
