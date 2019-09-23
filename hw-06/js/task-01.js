f38c7a9131c2c84edd31cdfcafcf67b130f0574e
Evgeniy <evgeniygal@gmail.com>
2019-09-23 21:35:35 +0300

    Hw-06 task-01

--- hw-06/js/task-01.js
+++ hw-06/js/task-01.js
@@ -1,15 +1,16 @@
-import users from './users';
+import users from './users.js';
+
+console.log(users);
 
 // const users = require('./users');
 
 // console.log(users);
-// const getUserNames = (users) => {
-//   // твой код
-// };
+const getUserNames = (users) => users.filter(item => item.name);
 // Получить массив имен всех пользователей (поле name).
 
 // console.log(getUserNames(users));
-// // [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
+// // [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr',
+// 'Blackburn Dotson', 'Sheree Anthony' ]
 // Задание 2
 
 // console.group('Task-01');
