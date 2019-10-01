68cf352e4dd0809e93ab17e4ee9450a92338f14e
Evgeniy Galamaga <evgeniygal@gmail.com>
2019-10-01 13:16:43 +0300

    Stert w-07

---
+++ hw-07/js/main.js
@@ -0,0 +1,5 @@
+const mainTitle = document.createElement('h1');
+document.body.style.textAlign = 'center';
+// mainTitle.style.textAlign = 'center';
+mainTitle.textContent = 'Please choose task!';
+document.body.prepend(mainTitle);
