eec44c4790ff5c8846b40677fb6740e47bafb84c
Evgeniy Galamaga <evgeniygal@gmail.com>
2019-10-11 11:05:06 +0300

    Done hw-08 50 %

--- hw-08/js/main.js
+++ hw-08/js/main.js
@@ -1,3 +1,34 @@
-import imeges from './gallery-items.js';
+import galleryItems from './gallery-items.js';
 
-console.log(imeges);
+const gallery = document.querySelector('.gallery');
+const galleryItemArr = [];
+
+galleryItems.forEach((element) => {
+  const { preview, original, description } = element;
+  const galleryItem = document.createElement('li');
+  galleryItem.classList.add('gallery__item');
+  const galleryItemLink = document.createElement('a');
+  galleryItemLink.href = original;
+  galleryItemLink.classList.add('gallery__link');
+  const image = document.createElement('img');
+  image.classList.add('gallery__image');
+  image.src = preview;
+  image.alt = description;
+  image.dataset.source = original;
+  const icon = document.createElement('span');
+  const iconMaterial = document.createElement('i');
+  icon.classList.add('gallery__icon');
+  iconMaterial.textContent = 'zoom_out_map';
+  iconMaterial.classList.add('material-icons');
+  icon.append(iconMaterial);
+  galleryItemLink.append(image, icon);
+  galleryItem.append(galleryItemLink);
+  galleryItemArr.push(galleryItem);
+});
+
+gallery.append(...galleryItemArr);
+
+
+console.log(gallery);
+
+console.log(galleryItems);
