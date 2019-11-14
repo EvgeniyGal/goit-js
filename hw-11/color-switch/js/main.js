457d10a4efdb442625aa41d3f26a759acee31a2f
Evgeniy Galamaga <evgeniygal@gmail.com>
2019-11-14 16:07:45 +0200

    Part 1  hw-11

--- hw-11/color-switch/js/main.js
+++ hw-11/color-switch/js/main.js
@@ -1,25 +1,21 @@
 import utils from './utils.js';
 
+const refs = {
+  btnStart: document.querySelector('button[data-action="start"]'),
+  btnStop: document.querySelector('button[data-action="stop"]'),
+  body: document.querySelector('body'),
+};
 
-const btnStart = document.querySelector('button[data-action="start"]');
-const btnStop = document.querySelector('button[data-action="stop"]');
-const body = document.querySelector('body');
-
-function changeBodyColor() {
-  body.style.backgroundColor = utils.colors[utils.randomIntegerFromInterval(0,
+const changeBodyColor = () => {
+  refs.body.style.backgroundColor = utils.colors[utils.randomIntegerFromInterval(0,
     utils.colors.length - 1)];
-  console.log('hi');
-}
+};
 
 let timer = null;
 
-function startChangeColor() {
-  timer = setInterval(changeBodyColor, 1000);
-}
+const startChangeColor = () => { timer = setInterval(changeBodyColor, 1000); };
 
-function stopChangeColor() {
-  clearInterval(timer);
-}
+const stopChangeColor = () => { clearInterval(timer); };
 
-btnStart.addEventListener('click', startChangeColor);
-btnStop.addEventListener('click', stopChangeColor);
+refs.btnStart.addEventListener('click', startChangeColor);
+refs.btnStop.addEventListener('click', stopChangeColor);
