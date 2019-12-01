b37c34e1cb831e8d309c77865c73155a0c69f8bc
Evgeniy <evgeniygal@gmail.com>
2019-12-01 21:15:31 +0200

    Start hw-11 timer

---
+++ hw-11/timer/src/js/count_down_timer.js
@@ -0,0 +1,13 @@
+class CountDownTimer {
+  constructor(targetDate) {
+    this.targetDate = new Date(targetDate);
+  }
+
+  calcDiff() {
+      return diffObj {
+          
+      }
+  }
+}
+
+module.exports = CountDownTimer;
